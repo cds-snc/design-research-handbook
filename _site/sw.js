97635fa64fd6037228eee8134f34d71fab665e50
Colin MacArthur <colinpmacarthur@fastmail.fm>
2019-02-08 20:52:45 -0500

    Adding table of contents

--- _site/sw.js
+++ _site/sw.js
@@ -1,17 +1,17 @@
-importScripts( '/design-research-handbook/cache-polyfill.js' );
+importScripts( '/cache-polyfill.js' );
 
 var filesToCache = [
   // root
-  '/design-research-handbook/',
-  '/design-research-handbook/index.html',
+  '/',
+  '/index.html',
   // css
-  '/design-research-handbook/assets/css/main.css',
-  '/design-research-handbook/assets/css/normalize.css',
-  '/design-research-handbook/assets/css/syntax.css',
+  '/assets/css/main.css',
+  '/assets/css/normalize.css',
+  '/assets/css/syntax.css',
   // images
-  '/design-research-handbook/assets/img/octocat.png',
+  '/assets/img/octocat.png',
   // pages
-  '/design-research-handbook/qrr/','/design-research-handbook/bibliography/','/design-research-handbook/protected-files/','/design-research-handbook/field-safety/','/design-research-handbook/airtable/','/design-research-handbook/bibliography/','/design-research-handbook/channels/','/design-research-handbook/contact/','/design-research-handbook/design-researcher-job-desc/','/design-research-handbook/field-safety/','/design-research-handbook/qrr/','/design-research-handbook/hiring-process/','/design-research-handbook/home/','/design-research-handbook/interviewing-basics/','/design-research-handbook/bibliography/','/design-research-handbook/okrs/','/design-research-handbook/bibliography/','/design-research-handbook/por/','/design-research-handbook/privacy/','/design-research-handbook/prof-development/','/design-research-handbook/quantifying/','/design-research-handbook/recruiting-participants/','/design-research-handbook/team-meetings/','/design-research-handbook/think-talk/','/design-research-handbook/translation/','/design-research-handbook/contact/','/design-research-handbook/accueil/','/design-research-handbook/coordonnees/',
+  '/qrr/','/bibliography/','/protected-files/','/field-safety/','/airtable/','/bibliography/','/channels/','/contact/','/design-researcher-job-desc/','/field-safety/','/qrr/','/hiring-process/','/home/','/interviewing-basics/','/bibliography/','/okrs/','/bibliography/','/por/','/privacy/','/prof-development/','/quantifying/','/recruiting-participants/','/team-meetings/','/think-talk/','/translation/','/contact/','/accueil/','/coordonnees/',
   // posts
   
 ];
